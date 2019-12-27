339a37bf399d319d007aee8c0f9b935e0fd714cd
Nixcholas <250458821@qq.com>
2019-12-27 18:00:56 +0800

    xk

--- report/src/main/webapp/WEB-INF/views/listStudent.jsp
+++ report/src/main/webapp/WEB-INF/views/listStudent.jsp
@@ -7,81 +7,15 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>学生列表</title>
     <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="/layui/css/layui.css">
     <script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="/layui/css/layui.css">
     <script type="text/javascript" src="/layui/layui.js"></script>
     <script type="text/javascript" src="/js/listStudent.js"></script>
 </head>
 <body>
+<%-- 列表 --%>
 <div>
-    <table id="student" lay-filter="test"></table>
-</div>
-<button id="exportStu" type="button" class="layui-btn layui-btn-primary">导出</button>
-<div id="addTable" hidden="hidden">
-    <%--学号：<input id="studentId" type='text'>--%>
-    <div class="layui-form-item">
-        <label class="layui-form-label">学号</label>
-        <div class="layui-input-block">
-            <input id="studentId" type="text" name="username" lay-verify="required" lay-reqtext="学号是必填项，岂能为空？"
-                   placeholder="请输入" autocomplete="off" class="layui-input">
-        </div>
-    </div>
-    <%--姓名：<input id="name" type='text'>--%>
-    <div class="layui-form-item">
-        <label class="layui-form-label">姓名</label>
-        <div class="layui-input-block">
-            <input type="text" id="name" name="username" lay-verify="required" lay-reqtext="姓名是必填项，岂能为空？"
-                   placeholder="请输入" autocomplete="off" class="layui-input">
-        </div>
-    </div>
-    <%--性别：<select id="sex">
-        <option value="男">男</option>
-        <option value="女">女</option>
-        </select>--%>
-    <div class="layui-form-item">
-        <label class="layui-form-label">性别</label>
-        <div class="layui-input-block">
-            <select id="sex" name="interest" lay-filter="aihao">
-                <option value=""></option>
-                <option value="男">男</option>
-                <option value="女">女</option>
-            </select>
-        </div>
-    </div>
-    <%--年龄：<input id="age" type='number'>--%>
-    <div class="layui-form-item">
-        <label class="layui-form-label">年龄</label>
-        <div class="layui-input-block">
-            <input id="age" type="number" name="username" lay-verify="required" lay-reqtext="年龄是必填项，岂能为空？"
-                   placeholder="请输入" autocomplete="off" class="layui-input">
-        </div>
-    </div>
-    <%--生日：<input id="birthday" type='date'>--%>
-    <div class="layui-inline">
-        <label class="layui-form-label">生日</label>
-        <div class="layui-input-inline">
-            <input type="text" name="date" id="birthday" lay-verify="date" placeholder="yyyy-MM-dd" autocomplete="off"
-                   class="layui-input">
-        </div>
-    </div>
-</div>
-    <div>
-        <%--<input id="showAdd" type="button" value="添加学生">--%>
-
-        <button id="addStudent" type="button" class="layui-btn layui-btn-primary" hidden="hidden">提交</button>
-        <button id="cancelAdd" type="button" class="layui-btn layui-btn-primary" hidden="hidden">取消</button>
-        <%--<input id="addStudent" type="button" value="提交" hidden="hidden">
-        <input id="cancelAdd" type="button" value="取消" hidden="hidden">--%>
-    </div>
-<%--    <div id="showImport">--%>
-<%--        导入:<input id="stuFile" type="file" multiple="multiple">--%>
-<%--        &lt;%&ndash;    <button id="stuFile" class="layui-btn" type="file" multiple="multiple">导入</button>&ndash;%&gt;--%>
-<%--&lt;%&ndash;        <input id="importStu" type="button" value="导入">&ndash;%&gt;--%>
-<%--        <button id="importStu" type="button" class="layui-btn layui-btn-primary">导入</button>--%>
-<%--    </div>--%>
-<div class="layui-upload" id="showImport">
-    <button id="stuFile" type="button" class="layui-btn layui-btn-normal">选择文件</button>
-    <button id="importStu" type="button" class="layui-btn">开始上传</button>
+    <table id="stuList" lay-filter="test"></table>
 </div>
 </body>
 <script type="text/html" id="toolbarDemo">
@@ -89,7 +23,9 @@
         <button class="layui-btn layui-btn-sm" lay-event="getCheckData">获取选中行数据</button>
         <button class="layui-btn layui-btn-sm" lay-event="getCheckLength">获取选中数目</button>
         <button class="layui-btn layui-btn-sm" lay-event="isAll">验证是否全选</button>
-        <button id="showAdd" type="button" class="layui-btn layui-btn-sm">添加学生</button>
+        <button id="addStu" type="button" class="layui-btn layui-btn-sm">添加学生</button>
+        <button id="importStu" type="button" class="layui-btn layui-btn-sm">导入学生</button>
+<%--        <button id="exportStu" type="button" class="layui-btn layui-btn-sm">导出学生</button>--%>
     </div>
 <script type="text/html" id="barDemo">
     <a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>
--- report/src/main/webapp/js/listStudent.js
+++ report/src/main/webapp/js/listStudent.js
@@ -1,249 +1,183 @@
-window.onload=function(){
-    layui.use(['form', 'layedit', 'laydate'], function(){
+window.onload = function () {
+    layui.use(['form', 'layedit', 'laydate','upload','table','layer'], function () {
         var form = layui.form
-            ,layer = layui.layer
-            ,layedit = layui.layedit
-            ,laydate = layui.laydate;
-
-        //日期
-        laydate.render({
-            elem: '#birthday'
-        });
-        laydate.render({
-            elem: '#date1'
-        });
-
-        //创建一个编辑器
-        var editIndex = layedit.build('LAY_demo_editor');
-
-        //自定义验证规则
-        form.verify({
-            title: function(value){
-                if(value.length < 5){
-                    return '标题至少得5个字符啊';
-                }
-            }
-            ,pass: [
-                /^[\S]{6,12}$/
-                ,'密码必须6到12位，且不能出现空格'
-            ]
-            ,content: function(value){
-                layedit.sync(editIndex);
-            }
-        });
-
-        //监听指定开关
-        form.on('switch(switchTest)', function(data){
-            layer.msg('开关checked：'+ (this.checked ? 'true' : 'false'), {
-                offset: '6px'
-            });
-            layer.tips('温馨提示：请注意开关状态的文字可以随意定义，而不仅仅是ON|OFF', data.othis)
-        });
-
-        //监听提交
-        form.on('submit(demo1)', function(data){
-            layer.alert(JSON.stringify(data.field), {
-                title: '最终的提交信息'
-            })
-            return false;
-        });
-
-        //表单赋值
-        layui.$('#LAY-component-form-setval').on('click', function(){
-            form.val('example', {
-                "username": "贤心" // "name": "value"
-                ,"password": "123456"
-                ,"interest": 1
-                ,"like[write]": true //复选框选中状态
-                ,"close": true //开关状态
-                ,"sex": "女"
-                ,"desc": "我爱 layui"
-            });
-        });
-
-        //表单取值
-        layui.$('#LAY-component-form-getval').on('click', function(){
-            var data = form.val('example');
-            alert(JSON.stringify(data));
-        });
-
-    });
-
-    layui.use('upload', function() {
-        var $ = layui.jquery
+            , layer = layui.layer
+            , laydate = layui.laydate
+            , $ = layui.jquery
+            , table = layui.table
+            , layer = layui.layer
             , upload = layui.upload;
-        //选完文件后不自动上传
-        upload.render({
-            elem: '#stuFile'
-            , url: '/import'
-            , auto: false
-            ,multiple: true
-            ,accept: 'file'
-            , bindAction: '#importStu'
-            , done: function (res) {
-                console.log(res);
-                layer.alert(res.msg);
-            }
+
+        //添加学生
+        $(document).on('click', '#addStu', function () {
+            layer.open({
+                type: 1,
+                skin: 'layui-layer-demo', //样式类名
+                // closeBtn: 0, //不显示关闭按钮
+                anim: 2,
+                area: ['800px', '500px'],
+                title: '添加学生',
+                btn: ['确定', '取消'],
+                shadeClose: false,
+                content: '<form class="layui-form" action=""><div id="addTable" style="margin-top: 5%;width:90%">\n' +
+                    '    <div class="layui-form-item">\n' +
+                    '        <label class="layui-form-label">学号</label>\n' +
+                    '        <div class="layui-input-block">\n' +
+                    '            <input id="studentId" type="text" name="username" lay-verify="required" lay-reqtext="学号是必填项，岂能为空？"\n' +
+                    '                   placeholder="请输入" autocomplete="off" class="layui-input">\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '    <div class="layui-form-item">\n' +
+                    '        <label class="layui-form-label">姓名</label>\n' +
+                    '        <div class="layui-input-block">\n' +
+                    '            <input type="text" id="name" name="username" lay-verify="required" lay-reqtext="姓名是必填项，岂能为空？"\n' +
+                    '                   placeholder="请输入" autocomplete="off" class="layui-input">\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '    <div class="layui-form-item">\n' +
+                    '        <label class="layui-form-label">性别</label>\n' +
+                    '        <div class="layui-input-block" id="sex">\n' +
+                    '            <input type="radio" name="sex" value="男" title="男" checked="">\n' +
+                    '            <input type="radio" name="sex" value="女" title="女">\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '    <div class="layui-form-item">\n' +
+                    '        <label class="layui-form-label">年龄</label>\n' +
+                    '        <div class="layui-input-block">\n' +
+                    '            <input id="age" type="number" name="username" lay-verify="required" lay-reqtext="年龄是必填项，岂能为空？"\n' +
+                    '                   placeholder="请输入" autocomplete="off" class="layui-input">\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '    <div class="layui-form-item">\n' +
+                    '        <label class="layui-form-label">生日</label>\n' +
+                    '        <div class="layui-input-inline">\n' +
+                    '            <input type="text" id="birthday" name="date" id="birthday" lay-verify="date" placeholder="yyyy-MM-dd" autocomplete="off"\n' +
+                    '                   class="layui-input">\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '</div></form>',
+                yes: function(index, layero){
+                    var studentId = $("#studentId").val();
+                    var name = $("#name").val();
+                    var sex = $('#sex input[name="sex"]:checked ').val();
+                    var age = $("#age").val();
+                    var birthday = $("#birthday").val();
+                    $.ajax({
+                        url: '/addStudent',
+                        type: 'post',
+                        data: {
+                            "studentId": studentId,
+                            "name": name,
+                            "sex": sex,
+                            "age": age,
+                            "birthday": birthday
+                        },
+                        success: function (data) {
+                            // $("#studentId").val('');
+                            // $("#name").val('');
+                            // $("#sex").val('');
+                            // $("#age").val('');
+                            // $("#birthday").val('');
+                            layer.msg(data.msg);
+                        }
+                    })
+                },
+                btn2: function(index, layero){
+                    layer.close(index); //如果设定了yes回调，需进行手工关闭
+                },
+            });
+            form.render();
+            //日期
+            laydate.render({
+                elem: '#birthday'
+                // ,value: new Date()
+            });
         });
-    })
 
-    layui.use('table', function(){
-        var table = layui.table;
-
+        //学生列表
         table.render({
-            elem: '#student'
-            ,cellMinWidth: 80 //全局定义常规单元格的最小宽度
-            ,url: '/listStudent' //数据接口
-            ,method:'get'
-            ,page: true //开启分页
-            ,skin: 'line' //行边框风格
-            ,even: true //开启隔行背景
-            ,size: 'lg' //小尺寸的表格
-            ,toolbar: '#toolbarDemo'
-            ,defaultToolbar: ['filter', 'exports', 'print', { //自定义头部工具栏右侧图标。如无需自定义，去除该参数即可
-                title: '提示'
-                ,layEvent: 'LAYTABLE_TIPS'
-                ,icon: 'layui-icon-tips'
-            }]
-            ,cols: [[ //表头
+            elem: '#stuList'
+            , cellMinWidth: 80 //全局定义常规单元格的最小宽度
+            , url: '/listStudent' //数据接口
+            , method: 'get'
+            ,page: {
+                layout: ['prev', 'page', 'next','count','limit', 'skip'] //自定义分页布局
+                ,groups: 3
+            }
+            , skin: 'line' //行边框风格
+            , even: true //开启隔行背景
+            , size: 'lg' //小尺寸的表格
+            , toolbar: '#toolbarDemo'
+            , defaultToolbar: ['filter', 'exports']
+            , cols: [[ //表头
                 {type: 'checkbox', fixed: 'left'}
-                ,{field: 'id', title: 'ID', sort: true, fixed: 'left'}
-                ,{field: 'studentId', title: '学号'}
-                ,{field: 'name', title: '姓名',sort: true}
-                ,{field: 'sex', title: '性别'}
-                ,{field: 'age', title: '年龄'}
-                ,{field: 'birthday', title: '生日', sort: true}
-                ,{fixed: 'right', title:'操作', toolbar: '#barDemo'}
+                , {field: 'id', title: 'ID', sort: true, fixed: 'left'}
+                , {field: 'studentId', title: '学号'}
+                , {field: 'name', title: '姓名', sort: true}
+                , {field: 'sex', title: '性别'}
+                , {field: 'age', title: '年龄'}
+                , {field: 'birthday', title: '生日', sort: true}
+                , {fixed: 'right', title: '操作', toolbar: '#barDemo'}
             ]]
         });
+
+        //导出学生
+        var exportStu = document.getElementById("exportStu");
+        exportStu.onclick = function () {
+            $.ajax({
+                url: '/export',
+                type: 'get',
+                success: function (data) {
+
+                }
+            })
+        }
+
+        //导入学生
+        upload.render({
+            elem: '#importStu'
+            ,url: '/import'
+            ,accept: 'file' //普通文件
+            ,done: function(res){
+                console.log(res);
+                layer.msg(res.msg);
+            }
+        });
+
         //头工具栏事件
-        table.on('toolbar(test)', function(obj){
+        table.on('toolbar(test)', function (obj) {
             var checkStatus = table.checkStatus(obj.config.id);
-            switch(obj.event){
+            switch (obj.event) {
                 case 'getCheckData':
                     var data = checkStatus.data;
                     layer.alert(JSON.stringify(data));
                     break;
                 case 'getCheckLength':
                     var data = checkStatus.data;
-                    layer.msg('选中了：'+ data.length + ' 个');
+                    layer.msg('选中了：' + data.length + ' 个');
                     break;
                 case 'isAll':
-                    layer.msg(checkStatus.isAll ? '全选': '未全选');
+                    layer.msg(checkStatus.isAll ? '全选' : '未全选');
                     break;
 
                 //自定义头工具栏右侧图标 - 提示
                 case 'LAYTABLE_TIPS':
                     layer.alert('这是工具栏右侧自定义的一个图标按钮');
                     break;
-            };
+            }
+            ;
         });
 
         //监听行工具事件
-        table.on('tool(test)', function(obj){
+        table.on('tool(test)', function (obj) {
             var data = obj.data;
             //console.log(obj)
-            if(obj.event === 'del'){
-                layer.confirm('真的删除行么', function(index){
+            if (obj.event === 'del') {
+                layer.confirm('真的删除行么', function (index) {
                     obj.del();
                     layer.close(index);
                 });
             }
         });
     });
-
-    var exportStu = document.getElementById("exportStu");
-    exportStu.onclick = function(){
-        $.ajax({
-            url : '/export',
-            type : 'get',
-            success : function(data) {
-
-            }
-        })
-    }
-
-    var showAdd = document.getElementById("showAdd");
-    showAdd.onclick = function(){
-        $("#addTable").show();
-        $("#showAdd").hide();
-        $("#showImport").hide();
-        $("#addStudent").show();
-        $("#cancelAdd").show();
-    }
-
-
-    var addStudent = document.getElementById("addStudent");
-    addStudent.onclick = function(){
-        var studentId = $("#studentId").val();
-        var name = $("#name").val();
-        var sex = $("#sex option:selected").val();
-        var age = $("#age").val();
-        var birthday = $("#birthday").val();
-        $.ajax({
-            url : '/addStudent',
-            type : 'post',
-            data:{
-                "studentId":studentId,
-                "name":name,
-                "sex":sex,
-                "age":age,
-                "birthday":birthday
-            },
-            success : function(data) {
-                $("#studentId").val('');
-                $("#name").val('');
-                $("#sex").val('');
-                $("#age").val('');
-                $("#birthday").val('');
-                alert(data.msg);
-            }
-        })
-    }
-
-    var cancelAdd = document.getElementById("cancelAdd");
-    cancelAdd.onclick = function(){
-        $("#showAdd").show();
-        $("#showImport").show();
-        $("#addStudent").hide();
-        $("#cancelAdd").hide();
-        $("#addTable").hide();
-    }
-
-    // var importStu = document.getElementById("importStu");
-    // importStu.onclick = function(){
-    //     var $file = $("#stuFile").val();//文件
-    //     // 判断文件是否为空
-    //     if ($file == "") {
-    //         alert("请选择上传的目标文件! ")
-    //         return false;
-    //     }
-    //     //判断文件类型
-    //     var fileName = $file.substring($file.lastIndexOf(".") + 1).toLowerCase();
-    //     if(fileName != "xls" && fileName !="xlsx"){
-    //         alert("请选择Excel文件!");
-    //         return false;
-    //     }
-    //     //判断文件大小
-    //     var size = $("#stuFile")[0].files[0].size;
-    //     if (size>104857600) {
-    //         alert("上传文件不能大于100M!");
-    //         return false;
-    //     }
-    //
-    //     var formData = new FormData();
-    //     formData.append("file",$('#stuFile')[0].files[0]);
-    //     $.ajax({
-    //         url : '/import',
-    //         type : 'post',
-    //         dataType:'json',
-    //         data:formData,
-    //         processData : false, // 使数据不做处理
-    //         contentType : false, // 不要设置Content-Type请求头
-    //         success : function(data) {
-    //             alert(data.msg);
-    //             // location.reload();
-    //         }
-    //     })
-    // }
 }
