7c840aabc8445f08e6cf59c912c1ed2d8368020c
cp3ugar <46948334+cp3ugar@users.noreply.github.com>
2020-08-27 14:24:23 +0800

    style(controller):remove some unusing improts
    Just removing some imports
    
    Close: #000

--- report/src/main/java/indi/xk/report/controller/BloggerController.java
+++ report/src/main/java/indi/xk/report/controller/BloggerController.java
@@ -1,6 +1,5 @@
 package indi.xk.report.controller;
 
-import indi.xk.report.pojo.AjaxResponse;
 import indi.xk.report.pojo.Blogger;
 import indi.xk.report.pojo.User;
 import indi.xk.report.service.BloggerService;
@@ -9,14 +8,11 @@ import indi.xk.report.utils.ImageVerificationCode;
 import indi.xk.report.utils.ReturnObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
